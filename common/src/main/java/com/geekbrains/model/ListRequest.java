f59ab34a82b77cee1816bd97e49d1b9401fee548
aditone <aditone2378@gmail.com>
2022-09-27 22:49:32 +0300

    Initial commit

---
+++ common/src/main/java/com/geekbrains/model/ListRequest.java
@@ -0,0 +1,18 @@
+package com.geekbrains.model;
+
+import lombok.Getter;
+
+@Getter
+public class ListRequest implements CloudMessage{
+
+    private final String selectedItem;
+
+    public ListRequest(String selectedItem) {
+        this.selectedItem = selectedItem;
+    }
+
+    @Override
+    public MessageType getType() {
+        return MessageType.UPDATE_SERVER_VIEW;
+    }
+}
---
+++ common/src/main/java/com/geekbrains/model/MessageType.java
@@ -0,0 +1,8 @@
+package com.geekbrains.model;
+
+public enum MessageType {
+    LIST,
+    FILE_REQUEST,
+    FILE,
+    UPDATE_SERVER_VIEW
+}
